465718bed1ea5d606a0399dd8f296b07b0061f09
Kevin Reynolds <Kevin.T.Reynolds30@gmail.com>
2017-09-26 20:32:50 -0700

    test menu icon functionality

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -65,12 +65,18 @@ $(function() {
          it('menu element is hidden', function() {
            expect($('body').hasClass('menu-hidden')).toBe(true);
          });
-         
+
          /* TODO: Write a test that ensures the menu changes
           * visibility when the menu icon is clicked. This test
           * should have two expectations: does the menu display when
           * clicked and does it hide when clicked again.
           */
+          it('menu changes visibility when menu icon is clicked', function () {
+            $('.menu-icon-link').click();
+            expect($('body').hasClass('menu-hidden')).toBe(false);
+            $('.menu-icon-link').click();
+            expect($('body').hasClass('menu-hidden')).toBe(true);
+          });
     });
     /* TODO: Write a new test suite named "Initial Entries" */
     describe('Initial Entries', function() {
